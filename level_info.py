f09f98c3ab670e14543b6ef8561c78831314ee08
Doodle <skorikovgk@yandex.ru>
2022-01-05 12:50:59 +0300

    111

---
+++ level_info.py
@@ -0,0 +1,18 @@
+level_map = [
+    '...........................',
+    '...........................',
+    '...........................',
+    'XXX...XXXXX.XX.........XXX ',
+    '..................XX.......',
+    'XXXX...X....XXX......XXXXXX',
+    'X.........XXXXX.....XXXXXXX',
+    'XXXXXX.............XXXX...X',
+    'XXXXXXXX...XXXXXX..XXXXXXXX',
+    'XXXXXXXX....XXXX...XXX.....',
+    'XXXXXXXX....XXXX...XXXXXXXX']
+
+tile_size = 60
+SCREEN_WIDTH = 1100
+SCREEN_HEIGHT = len(level_map) * tile_size
+
+

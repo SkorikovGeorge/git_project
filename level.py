aa1905c590a7616ce87f95976cd6abba5043b655
Doodle <skorikovgk@yandex.ru>
2022-01-05 12:50:47 +0300

    111

---
+++ level.py
@@ -0,0 +1,22 @@
+import pygame
+from levels_tiles import Tile
+from level_info import tile_size
+
+
+class Level:
+    def __init__(self, level_map_data, surface):
+        self.tiles_sprite_group = pygame.sprite.Group()
+        self.display_surface = surface
+        self.level_building(level_map_data)
+
+    def level_building(self, map_data):
+        for row_index, row in enumerate(map_data):
+            for col_index, column in enumerate(row):
+                if column == 'X':
+                    x = col_index * tile_size
+                    y = row_index * tile_size
+                    tile = Tile((x, y), tile_size)
+                    self.tiles_sprite_group.add(tile)
+
+    def run(self):
+        self.tiles_sprite_group.draw(self.display_surface)

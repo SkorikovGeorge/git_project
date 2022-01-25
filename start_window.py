f706ae410f6cce4769390d51c9693af9acced94b
Alena <azabuselova@gmail.com>
2022-01-25 17:37:08 +0300

    надпись на стартовом экране

--- start_window.py
+++ start_window.py
@@ -9,6 +9,15 @@ class Start:
         self.background_image = pygame.image.load('images/space_image/space_background.jpg')
         self.background_image = pygame.transform.scale(self.background_image, (
             self.background_image.get_width() * 3, self.background_image.get_height() * 2))
+        font = pygame.font.Font(None, 50)
+        self.text = font.render("название игры", True, 'yellow')
+        self.text_w = self.text.get_width()
+        self.text_h = self.text.get_height()
+        self.text_x = SCREEN_WIDTH // 2 - self.text_w // 2
+        self.text_y = SCREEN_HEIGHT // 5
 
     def run(self):
         self.display_surface.blit(self.background_image, (0, 0))
+        self.display_surface.blit(self.text, (self.text_x, self.text_y))
+        pygame.draw.rect(self.display_surface, 'yellow', (self.text_x - 10, self.text_y - 10,
+                                                          self.text_w + 20, self.text_h + 20), 1)

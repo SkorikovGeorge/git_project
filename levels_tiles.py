24cc84f40d275cb87e2bee6e0f39c315fc15f692
Георгий <skorikovgk@yandex.ru>
2022-01-25 17:20:03 +0300

    добавил спрайт корабля, сделал так, чтобы он поворачивался, добавил еще один спрайт метеоритика, заранее добавил спрайт убивающего метеорита

--- levels_tiles.py
+++ levels_tiles.py
@@ -4,7 +4,9 @@ import random
 tile_image = pygame.image.load('images/tile_image/метеорит.png')
 tile_image_1 = pygame.image.load('images/tile_image/метеорит1.png')
 tile_image_2 = pygame.image.load('images/tile_image/метеорит2.png')
-tile_images = [tile_image, tile_image_1, tile_image_2]
+tile_image_3 = pygame.image.load('images/tile_image/метеорит3.png')
+lava_tile_image = pygame.image.load('images/tile_image/горячий_метеорит.png')
+tile_images = [tile_image, tile_image_1, tile_image_2, tile_image_3]
 
 
 class Tile(pygame.sprite.Sprite):
@@ -14,7 +16,7 @@ class Tile(pygame.sprite.Sprite):
             self.image = pygame.Surface((size, size))
             self.rect = self.image.get_rect(topleft=position)
         else:
-            image_number = random.randint(1, 3)
+            image_number = random.randint(1, 4)
             self.image = pygame.transform.scale(tile_images[image_number - 1], (size, size))
             self.rect = self.image.get_rect(topleft=position)
 
--- player.py
+++ player.py
@@ -1,15 +1,26 @@
 import pygame
 from level_info import tile_size
 
-# ship_image_left_on
+player_spaceship_image = pygame.image.load('images/spaceship_images/корабль выкл.png')
+player_diagonal_spaceship_image = pygame.image.load('images/spaceship_images/корабль диагональ выкл.png')
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        self.image = pygame.Surface((tile_size * 0.65, tile_size * 0.65))
-        self.image.fill('violet')
+        self.image = pygame.transform.scale(player_spaceship_image, (tile_size * 0.8, tile_size * 0.8))
         self.rect = self.image.get_rect(topleft=pos)
+
+        self.spaceship_up = self.image
+        self.spaceship_down = pygame.transform.flip(self.image, False, True)
+        self.spaceship_left = pygame.transform.rotate(self.image, 90)
+        self.spaceship_right = pygame.transform.rotate(self.image, -90)
+
+        self.spaceship_up_and_right = pygame.transform.rotate(self.image, -45)
+        self.spaceship_up_and_left = pygame.transform.rotate(self.image, 45)
+        self.spaceship_down_and_right = pygame.transform.rotate(self.image, -135)
+        self.spaceship_down_and_left = pygame.transform.rotate(self.image, 135)
+
         self.direction = pygame.math.Vector2(0, 0)
         self.speed_x = 8
         self.speed_y = 8
@@ -30,7 +41,14 @@ class Player(pygame.sprite.Sprite):
             self.direction.y = 0
 
     def rotate_player_sprite(self):
-        pass
+        if self.direction.y == 1:
+            self.image = self.spaceship_down
+        elif self.direction.y == -1:
+            self.image = self.spaceship_up
+        elif self.direction.x == 1:
+            self.image = self.spaceship_right
+        elif self.direction.x == -1:
+            self.image = self.spaceship_left
 
     def update(self):
         self.moving()
